f983ce58ea7d2b5b783add26832f72d1a5163c38
soham1111 <roysoham0007@gmail.com>
2020-11-02 18:50:46 +0530

    ygvu

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(carimg1);
     car2 = createSprite(300,200);
+    car2.addImage(carimg2);
     car3 = createSprite(500,200);
+    car3.addImage(carimg3);
     car4 = createSprite(700,200);
+    car4.addImage(carimg4);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,13 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background(rgb(190,135,103));
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5)
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -78,7 +83,12 @@ class Game {
       player.distance +=10
       player.update();
     }
-
+    if (player.distance>3850){
+      gameState=2
+    }
     drawSprites();
+  }
+  end(){
+    console.log("Game End")
   }
 }
--- sketch.js
+++ sketch.js
@@ -9,7 +9,15 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
+var carimg1,carimg2,carimg3,carimg4,track, ground;
+function preload(){
+  track = loadImage("images/track.png");
+  carimg1=loadImage("images/car1.png");
+  carimg2=loadImage("images/car2.png");
+  carimg3=loadImage("images/car3.png");
+  carimg4=loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +35,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end();
   }
 }
